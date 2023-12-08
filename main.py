00775f84e936ae225b77e7a259d27ce4af96cce6
YoungHun <younghun4218@gmail.com>
2023-12-09 00:34:33 +0900

    파일 경로 오류 수정

--- main.py
+++ main.py
@@ -3,30 +3,35 @@ import numpy as np
 import os
 from scipy.spatial.distance import cosine
 
-
 def extract_color_histogram(image_path):
-    image = cv2.imread(image_path)
+    
+    img_array = np.fromfile(image_path, np.uint8)
+    image = cv2.imdecode(img_array, cv2.IMREAD_COLOR) #cv.imread 한글 경로 인식 안되는 문제를 해결하기 위해 넘파이 array로 먼저 받아오기
+    
+    
+    if image is None:
+        raise ValueError(f"Unable to load image at path: {image_path}")
     histogram = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
     cv2.normalize(histogram, histogram)
     return histogram.flatten()
 
 def compare_histograms(hist1, hist2):
     return cosine(hist1, hist2)
-
+current_script_directory = os.getcwd()
 # Specify the folder path
-folder_path = 'images'
-
+folder_path = os.path.join(current_script_directory, 'images')
+target_path = os.path.join(current_script_directory, 'target.jpg')
 # Retrieve and filter image files
 image_extensions = ['.jpg', '.jpeg', '.png', '.bmp']
 image_paths = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.splitext(f)[1].lower() in image_extensions]
 
 # Extract histograms
 histograms = [extract_color_histogram(path) for path in image_paths]
-target_histogram = extract_color_histogram('target.png')
-# Compare each images
+target_histogram = extract_color_histogram(target_path)
 
+# Compare each image with the target
 similarities = [compare_histograms(target_histogram, histogram) for histogram in histograms]
 
-# Find the most similar pair
-most_similar_pair = min(similarities, key=similarities.get)
-print(f"The most similar images are: {image_paths[most_similar_pair[0]]} and {image_paths[most_similar_pair[1]]} with a similarity score of {similarities[most_similar_pair]}")
+# Find the most similar image
+most_similar_image_index = np.argmin(similarities)
+print(f"The most similar image to target is: {image_paths[most_similar_image_index]} with a similarity score of {similarities[most_similar_image_index]}")
