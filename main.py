2ab1bccd48a3f955552403b361aaffa05e58ca8e
YoungHun <younghun4218@gmail.com>
2023-12-08 21:46:19 +0900

    cluster 방식이 이 문제 해결에 적합하지 않음을 깨닫고 Cosine Similarity 방법으로 구분함

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
-from sklearn.cluster import KMeans
-from matplotlib import pyplot as plt
+from scipy.spatial.distance import cosine
+from itertools import combinations
 
 def extract_color_histogram(image_path):
     image = cv2.imread(image_path)
@@ -9,18 +9,19 @@ def extract_color_histogram(image_path):
     cv2.normalize(histogram, histogram)
     return histogram.flatten()
 
-# Load images and extract features
-image_paths = ['path/to/image1.jpg', 'path/to/image2.jpg', '...']
-features = np.array([extract_color_histogram(path) for path in image_paths])
+def compare_histograms(hist1, hist2):
+    return cosine(hist1, hist2)
 
-# Clustering
-kmeans = KMeans(n_clusters=3)  # Adjust the number of clusters
-labels = kmeans.fit_predict(features)
+# Paths to your images
+image_paths = ['image1.jpg', 'image2.jpg', 'image3.jpg', ...]
 
-# Visualize the results
-for i in range(len(labels)):
-    plt.subplot(1, len(labels), i + 1)
-    plt.imshow(cv2.imread(image_paths[i]))
-    plt.title(f'Cluster {labels[i]}')
-    plt.axis('off')
-plt.show()
+# Extract histograms
+histograms = [extract_color_histogram(path) for path in image_paths]
+
+# Compare each pair of images
+pairs = combinations(range(len(histograms)), 2)
+similarities = {pair: compare_histograms(histograms[pair[0]], histograms[pair[1]]) for pair in pairs}
+
+# Find the most similar pair
+most_similar_pair = min(similarities, key=similarities.get)
+print(f"The most similar images are: {image_paths[most_similar_pair[0]]} and {image_paths[most_similar_pair[1]]} with a similarity score of {similarities[most_similar_pair]}")
