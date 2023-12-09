c25401a3ddfa36036e5e391fdbce4b1607a98033
YoungHun <younghun4218@gmail.com>
2023-12-10 04:12:10 +0900

    웹에서 파일을 업로드하고 판별할수 있도록 Flask를 이용해 코드 수정

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torchvision import models, transforms
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 # Function to extract features using a pre-trained ResNet model
-def extract_resnet_features(image_path, model):
-    img_array = np.fromfile(image_path, np.uint8)
-    image = cv2.imdecode(img_array, cv2.IMREAD_COLOR) #cv.imread 한글 경로 인식 안되는 문제를 해결하기 위해 넘파이 array로 먼저 받아오기    
+def extract_resnet_features(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert to RGB
     image = cv2.resize(image, (224, 224))  # Resize to match ResNet input size
     image = transforms.ToTensor()(image).unsqueeze(0)  # Convert to PyTorch tensor
@@ -27,29 +28,44 @@ model.eval()
 current_script_directory = os.getcwd()
 folder_path = os.path.join(current_script_directory, 'images2')
 
-
 # Load and extract features for pre-stored clothing photos
 pre_stored_features = {}
 for file_name in os.listdir(folder_path):
     file_path = os.path.join(folder_path, file_name)
-    image_features = extract_resnet_features(file_path, model)
+    img_array = np.fromfile(file_path, np.uint8)
+    image = cv2.imdecode(img_array, cv2.IMREAD_COLOR) #cv.imread 한글 경로 인식 안되는 문제를 해결하기 위해 넘파이 array로 먼저 받아오기    
+    image_features = extract_resnet_features(image)
     pre_stored_features[file_name] = image_features
 
-# Define the target clothing photo
-target_photo_path = os.path.join(current_script_directory, 'target.jpg')
+@app.route('/upload', methods=['POST'])
+def upload_target_image():
+    try:
+        target_image = request.files['fileToUpload']
+        if target_image:
+            # Convert uploaded image to numpy array
+            target_image_data = np.frombuffer(target_image.read(), np.uint8)
+            target_image = cv2.imdecode(target_image_data, cv2.IMREAD_COLOR)
 
-# Extract features for the target clothing photo
-target_features = extract_resnet_features(target_photo_path, model)
+            # Extract features for the target clothing photo
+            target_features = extract_resnet_features(target_image)
 
-# Calculate similarity between the target photo and pre-stored photos
-similarities = {}
-for file_name, features in pre_stored_features.items():
-    similarity = np.dot(target_features, features) / (np.linalg.norm(target_features) * np.linalg.norm(features))
-    similarities[file_name] = similarity
+            # Calculate similarity between the target photo and pre-stored photos
+            similarities = {}
+            for file_name, features in pre_stored_features.items():
+                similarity = np.dot(target_features, features) / (np.linalg.norm(target_features) * np.linalg.norm(features))
+                similarities[file_name] = similarity
 
-# Find the closest pre-stored clothing photo
-closest_photo = max(similarities, key=similarities.get)
-similarity_score = similarities[closest_photo]
-ranked_photos = sorted(similarities.items(), key=lambda x: x[1], reverse=True)
-for rank, (photo, similarity_score) in enumerate(ranked_photos, start=1):
-    print(f"Rank {rank}: {photo} (Similarity Score: {similarity_score})")
+            # Rank the pre-stored clothing photos by similarity
+            ranked_photos = sorted(similarities.items(), key=lambda x: x[1], reverse=True)
+
+            # Prepare the response
+            response_data = [{"photo": photo, "similarity_score": float(similarity_score)} for photo, similarity_score in ranked_photos]
+            print(response_data)
+            
+            return jsonify({"results": response_data})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
